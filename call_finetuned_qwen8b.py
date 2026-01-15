bf50cfcfb08d06a8669c833391bcd43aa61a36e8
sofiatabioye <abioye.sofiat@outlook.com>
2026-01-15 09:16:45 +0000

    update

--- call_finetuned_qwen8b.py
+++ call_finetuned_qwen8b.py
@@ -7,7 +7,7 @@ messages = [
     {
         "role": "user",
         "content": [
-            {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+            {"type": "image", "image": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
             {"type": "text", "text": "What animal is on the candy?"}
         ]
     },
